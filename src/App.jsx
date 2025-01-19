78a981c9278606cf8df16cfcf26a82dae4a74f91
ivand <ivan.d.kostov@gmail.com>
2025-01-19 21:49:09 +0200

    added: Header HomePage components and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header></Header>
+        <HomePage></HomePage>
+      </section>
+      <footer>
+          
+      </footer>
+    </div>
   )
 }
 
